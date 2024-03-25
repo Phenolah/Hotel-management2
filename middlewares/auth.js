889c6b7057a906587ca07e63960f3cc91b05e603
Phenolah <104977409+Phenolah@users.noreply.github.com>
2024-03-25 07:36:04 -0700

    Update auth.js

---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+
+// Middleware to authenticate users
+export const authenticateUser = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(400).json({ message: 'Invalid token.' });
+    }
+};
+
+// Middleware to authorize admin users
+export const authorizeAdmin = (req, res, next) => {
+    if (req.user && req.user.role === 'admin') {
+        next();
+    } else {
+        res.status(403).json({ message: 'Access denied. Admin privilege required.' });
+    }
+};
